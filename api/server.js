925459e2e5aefd391eac12183ec8973f3209be03
Steve <steve.l.chen@gmail.com>
2015-12-12 20:47:38 -0500

    api routes created server side

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+
 const path = require('path');
 const cors = require('cors');
 const logger = require('morgan');
@@ -8,11 +9,15 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
-
+app.use(express.static(path.join(__dirname, 'public')));
 mongoose.connect('mongodb://localhost:27017/podcasts')
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+let podcast = require('./controllers/podcasts_controller');
+app.use('/podcasts', podcast);
 
 app.listen(3000);
